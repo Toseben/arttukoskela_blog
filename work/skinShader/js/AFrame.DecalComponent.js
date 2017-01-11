628289fb309b2316928b1c7dd9ea9dcf3151e540
Arttu <arekoskela@gmail.com>
2017-01-11 21:31:39 +0200

    JPG and PNG custom image support

--- work/skinShader/js/AFrame.DecalComponent.js
+++ work/skinShader/js/AFrame.DecalComponent.js
@@ -2,11 +2,44 @@
 AFRAME.registerComponent('decal', {
     dependencies: [],
     schema: { 
-        scale: {default: 0.25},
+        scale: {default: 0.15},
         rotation: {default: 0}
     },
     
     init: function () {
+        
+        // Change Image
+        $("#userImage").change(function () {
+            // IMAGE
+            var image = document.createElement( 'img' );
+            var texture = new THREE.Texture( image );
+            image.onload = function()  {
+                    texture.needsUpdate = true;
+                    console.log("Loaded!");
+                    uniforms.map = { type: "t", value: texture};
+                    uniforms.shaderSwitch = { type: "i", value: shaderSwitch };
+                };
+
+            userImage = $("#userImage")[0];
+            
+            var imageType = userImage.value.split(".").pop();
+            
+            var shaderSwitch = false;
+            if (imageType === 'png'){
+                var shaderSwitch = true;
+            }
+                     
+            if (userImage.files && userImage.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    image.src = e.target.result;
+                };
+
+                reader.readAsDataURL(userImage.files[0]);
+            }
+        });
+        
         // Variables
         var mesh, ray_entity;
         var decals = [];
@@ -43,6 +76,39 @@ AFRAME.registerComponent('decal', {
             polygonOffsetFactor: -4, 
             wireframe: false 
         });
+        
+        // Custom Ink Material
+        var vertexShader = THREE.ShaderLib.phong.vertexShader;
+        var fragmentShader = getShader();
+        var uniforms = THREE.UniformsUtils.clone(THREE.ShaderLib.phong.uniforms);
+
+        var defines = {};
+        defines[ "USE_MAP" ] = "";
+        
+        var color_map = loader.load( 'img/colored.jpg' );
+        var normal_map = loader.load( 'img/ink_normal.jpg' );
+        
+        uniforms.specular = { type: "c", value: new THREE.Color( 0x3e444c )};
+        uniforms.shininess = { type: "f", value: 10 };
+        uniforms.map = { type: "t", value: color_map};
+        uniforms.normalMap = { type: "t", value: normal_map};
+        uniforms.normalScale = { type: "v2", value: new THREE.Vector2( 0.25, 0.25 )};
+        
+        uniforms.shaderSwitch = { type: "i", value: 0 };
+        
+        decalMaterial = new THREE.ShaderMaterial( {
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader,
+            uniforms: uniforms,
+            defines: defines,
+            lights: true,
+            fog: false,
+            transparent: true,
+            depthTest: true, 
+            depthWrite: false, 
+            polygonOffset: true,
+            polygonOffsetFactor: -4
+        } );
         
         // NORMAL MATERIAL
         /*decalMaterial = new THREE.MeshNormalMaterial( { 
